9668ca1a123380ecb62ac3d930fdcec650b15c4e
Soso Ogan <soso737@hotmail.co.uk>
2022-05-05 11:06:58 +0100

    fixed url.match is not a function error

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-// app.use(cors()) //add CORS support to each following route handler
+app.use(cors()) //add CORS support to each following route handler
 
 
 const client = new Client(dbConfig);
@@ -40,8 +40,9 @@ app.get("/", async (req, res) => {
   res.json(dbres.rows);
 });
 
-function urlExtracting(url:string):string[] {
-  const breedAndSubBreed= url.match(/.*\/(.*)\/(.*)$/)[1]
+function urlExtracting(url:{message: string}):string[] {
+ 
+  const breedAndSubBreed= url.message.match(/.*\/(.*)\/(.*)$/)[1]
   let breed
   let subBreed
   if (breedAndSubBreed.includes('-')){
@@ -55,11 +56,11 @@ function urlExtracting(url:string):string[] {
 }
 
 app.post("/", async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
+  // res.set('Access-Control-Allow-Origin', '*')
   const url = req.body.message
   const isPresent = await client.query('SELECT CASE WHEN EXISTS (SELECT * FROM vote WHERE image_url = $1)THEN $2 ELSE $3 END',[url,1,0]) //return true if url exists else false
   console.log(isPresent.rows[0].case)
-  console.log(url)
+  
   if(isPresent.rows[0].case==='0') {
   const[breed,subBreed]=urlExtracting(url)
   const dbres = await client.query('insert into vote (breed, sub_breed, image_url,vote_count) values($1,$2,$3,$4) returning sub_breed',[breed,subBreed,url,0]);
